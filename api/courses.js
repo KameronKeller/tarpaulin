c037cfbf1bd669828044c777e18e723c6c35221c
Devon Ceccacci <dceccacci@gmail.com>
2024-06-05 15:18:48 -0700

    can post students into a course

--- api/courses.js
+++ api/courses.js
@@ -52,4 +52,53 @@ router.delete('/:courseId', auth.authenticate, auth.authorize(["admin"]), async 
     }
 })
 
+/* POST /courses/{id}/students
+*   Enrolls and/or unenrolls students from a Course. Only an authenticated 
+*   User with 'admin' role or an authenticated 'instructor' User whose ID 
+*   matches the instructorId of the Course can update the students enrolled in the Course.
+*   
+*   Example Request:
+*   {
+*   "add": [
+*        "123",
+*        "456"
+*   ],
+*   "remove": [
+*       "123",
+*        "456"
+*     ]
+*   }
+*/
+router.post('/:courseId', auth.authenticate, auth.authorize(["admin", "instructor"]), async (req, res, next) => {
+    if(req.body.add || req.body.remove){
+        try {
+            const course = get_courses_by_id(req.params.courseId);
+            
+            if((req.role == "instructor" && course.instructorId == req.userId) || req.role == "admin"){
+                // An admin and an instructor with the same id as in the course can view the students
+                // Update the course's students with the id's found in the add or remove array.
+                update_students(req, course._id);
+                res.send(200).json({
+                    success: "Successfully updated courses students"
+                });
+            } else {
+                // Unauthorized
+                res.send(404).json({
+                    error: "Unauthorized"
+                })
+            }
+
+        } catch (error) {
+            res.status(500).json({
+                error: error
+            });
+        }
+    } else {
+        // incorrect fields passed
+        res.send(400).json({
+            error: "Invalid fields in request"
+        })
+    }
+})
+
 module.exports = router
--- model/courses.js
+++ model/courses.js
@@ -20,7 +20,7 @@ async function delete_course(courseId) {
 
     const db = getDbReference();
     const courseCollection = db.collection("Courses");
-    const courseResult = await courseCollection.deleteOne({ id: courseId });
+    const courseResult = await courseCollection.deleteOne({ _id: courseId });
     if(courseResult.acknowledged && courseResult.deletedCount === 1){
         const assignmentCollection = db.collection("Assignments");
         const assignmentResult= await assignmentCollection.deleteMany({ courseId: courseId });
@@ -39,7 +39,33 @@ exports.delete_course = delete_course
 async function get_courses_by_id(courseId){
     const db = getDbReference();
     const collection = db.collection("Courses");
-    const result = await collection.findOne({ id: courseId });
+    const result = await collection.findOne({ _id: courseId });
     return result;
 }
 exports.get_courses_by_id = get_courses_by_id
+
+/*
+*   Adds and removes students from a course
+*/
+async function update_students(req, courseId){
+    const db = getDbReference();
+    const collection = db.collection("Courses");
+
+    const toAdd = req.body.add;
+    if(toAdd.length > 0){
+        db.collection.updateOne(
+            { _id: courseId },
+            { $addToSet: { students: { $each: toAdd } } }
+        );
+    }
+    const toRemove = req.body.remove;
+    if(toRemove.length > 0){
+        db.collection.updateOne(
+            { _id: courseId },
+            { $pullAll: { students: { $each: toRemove } } }
+        );
+    }
+
+}
+
+exports.update_students = update_students
