0c1cd53979cabd2061d0a6d44ace9fa9b66ff523
Devon Ceccacci <dceccacci@gmail.com>
2024-06-05 14:08:28 -0700

    added courses delete

--- api/courses.js
+++ api/courses.js
@@ -1,8 +1,29 @@
 const { Router } = require('express')
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.status(200).send("Hello Courses")
+const auth = require('../lib/auth')
+const { delete_course } = require('../model/courses')
+
+
+/*
+ *  Completely removes the data for the specified Course, including all enrolled students, 
+ *  all Assignments, etc. Only an authenticated User with 'admin' role can remove a Course.
+*/
+router.delete('/:courseId', auth.authenticate, auth.authorize(["admin"]), async (req, res, next) => {
+    try {
+        result = await delete_course(req.params.courseId);
+        if(result == -1){
+            res.status(404).json({
+                error: "ID Not Found"
+            })
+        } else {
+            res.status(204).end();
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
 })
 
 module.exports = router
---
+++ model/courses.js
@@ -0,0 +1,33 @@
+const { ObjectId, GridFSBucket } = require('mongodb')
+const { getDbReference } = require('../lib/mongo')
+const auth = require('../lib/auth');
+
+const CoursesSchema = {
+    subjectCode: {required: true},
+    number: {required: true},
+    title: {required: true},
+    term: {required: true},
+    instructorId: {required: true},
+    students: {required: true}
+}
+
+
+/*
+ * Delete a course from the database
+ * Deletes any assignment's for that course.
+*/
+async function delete_course(courseId) {
+
+    const db = getDbReference();
+    const courseCollection = db.collection("Course");
+    const courseResult = await courseCollection.deleteOne({ id: courseId });
+    if(courseResult.acknowledged && courseResult.deletedCount === 1){
+        const assignmentCollection = db.collection("Assignment");
+        const assignmentResult= await assignmentCollection.deleteMany({ courseId: courseId });
+        if(assignmentResult.acknowledged && assignmentResult.deletedCount === 1){
+            return 0;
+        }
+    }
+    return -1;
+}
+exports.delete_course = delete_course
