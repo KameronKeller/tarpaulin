b30f0875cbdfe61a1a834631b84318f6bad8a15c
KameronKeller <mrkameronkeller@gmail.com>
2024-06-06 11:21:49 -0700

    Initial code for courses

--- api/courses.js
+++ api/courses.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
+const { authenticate, authorize, ROLES, isAuthorized } = require("../lib/auth");
 const router = Router();
 
 router.get("/", async (req, res) => {
   const page = parseInt(req.query.page) || 1;
-  const subject = req.query.subject
-  const courseNumber = req.query.number
-  const term = req.query.term
+  const subject = req.query.subject;
+  const courseNumber = req.query.number;
+  const term = req.query.term;
 
   const numPerPage = 10;
 
@@ -19,14 +20,25 @@ router.get("/", async (req, res) => {
       "term": "sp22",
       "instructorId": 123
     */
-//   const { count, rows } = await Business.findAndCountAll({
-//     limit: numPerPage,
-//     offset: page,
-//     include: [
-//       { model: Review, required: false },
-//       { model: Photo, required: false },
-//     ],
-//   });
+
+// Src for pagination: https://codebeyondlimits.com/articles/pagination-in-mongodb-the-only-right-way-to-implement-it-and-avoid-common-mistakes
+  const courses = await Courses.aggregate([
+    {
+      $facet: {
+        metadata: [{ $count: "totalCount" }],
+        data: [{ $skip: (page - 1) * pageSize }, { $limit: pageSize }],
+      },
+    },
+  ]);
+
+  //   const { count, rows } = await Course.findAndCountAll({
+  //     limit: numPerPage,
+  //     offset: page,
+  //     include: [
+  //       { model: Review, required: false },
+  //       { model: Photo, required: false },
+  //     ],
+  //   });
 
   const lastPage = Math.ceil(count / numPerPage);
   page = page > lastPage ? lastPage : page;
@@ -53,5 +65,83 @@ router.get("/", async (req, res) => {
     links: links,
   });
 });
+
+router.post(
+  "/",
+  authenticate,
+  authorize([ROLES.admin]),
+  async (req, res) => {
+    // TODO: update validation
+    // if (validateAgainstSchema(req.body, BusinessSchema)) {
+    try {
+      // const id = await insertNewCourse(req.body)
+      res.status(201).send({
+        id: id,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({
+        error: "Error inserting course into DB.  Please try again later.",
+      });
+    }
+    // } else {
+    //   res.status(400).send({
+    //     error: "Request body is not a valid course object.",
+    //   });
+  }
+  //   }
+);
+
+/*
+
+{
+  "subject": "CS",
+  "number": "493",
+  "title": "Cloud Application Development",
+  "term": "sp22",
+  "instructorId": 123
+}
+
+ */
+router.get("/:id", async (req, res, next) => {
+  try {
+    // const business = await getBusinessById(req.params.id);
+    const course = await getCourseById(req.params.id);
+    if (course) {
+      res.status(200).send(course);
+    } else {
+      next();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      error: "Unable to fetch business.  Please try again later.",
+    });
+  }
+});
+
+router.patch(
+  "/:id",
+  authenticate,
+  authorize([ROLES.admin, ROLES.instructor]),
+  async function (req, res, next) {
+    if (await isAuthorized(req)) {
+      const courseId = req.params.courseId;
+      const result = await Course.update(req.body, {
+        where: { id: courseId },
+        fields: CourseFields,
+      });
+      if (result[0] > 0) {
+        res.status(204).send();
+      } else {
+        next();
+      }
+    } else {
+      res.status(403).json({
+        error: "Unauthorized to access the specified resource",
+      });
+    }
+  }
+);
 
 module.exports = router;
