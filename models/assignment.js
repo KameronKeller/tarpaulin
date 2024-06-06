9af3e92fcc873cd36283c16aa0adc8ae6e143f8a
Micah Stalberg <stalberm@oregonstate.edu>
2024-06-06 16:22:57 -0700

    Fix assignmentschema typo

--- models/assignment.js
+++ models/assignment.js
@@ -1,71 +1,79 @@
-const {ObjectId, GridFSBucket} = require('mongodb');
-const {getDbReference} = require('../lib/mongo');
-const auth = require('../lib/auth');
-const { extractValidFields } = require('../lib/validation');
+const { ObjectId, GridFSBucket } = require("mongodb");
+const { getDbReference } = require("../lib/mongo");
+const auth = require("../lib/auth");
+const { extractValidFields } = require("../lib/validation");
 
 // courseId, title, points, due
-const AssignmnetSchema = {
-    courseId : {required: true},
-    title : {required: true},
-    points: {required: true},
-    due: {required: true}
-}
+const assignmentSchema = {
+  courseId: { required: true },
+  title: { required: true },
+  points: { required: true },
+  due: { required: true },
+};
 
 async function getAssignment(assignmentId) {
-    const db = getDbReference();
+  const db = getDbReference();
 
-    const assignment = await db.collection('Assignments').findOne({ _id: ObjectId.createFromHexString(assignmentId) });
-    if (!assignment) {
-        return null;
-    };
-    
-    return {
-        ...assignment
-    };
+  const assignment = await db
+    .collection("Assignments")
+    .findOne({ _id: ObjectId.createFromHexString(assignmentId) });
+  if (!assignment) {
+    return null;
+  }
+
+  return {
+    ...assignment,
+  };
 }
 
 exports.getAssignment = getAssignment;
 
 async function insertAssignment(assignmentInfo) {
-    const assignment = extractValidFields(assignmentInfo, AssignmnetSchema);
-    const db = getDbReference();
-    const collection = db.collection('Assignments');
-    const result = await collection.insertOne(assignment);
-    return {'id': result.insertedIds};
+  const assignment = extractValidFields(assignmentInfo, assignmentSchema);
+  const db = getDbReference();
+  const collection = db.collection("Assignments");
+  const result = await collection.insertOne(assignment);
+  return { id: result.insertedIds };
 }
 exports.insertAssignment = insertAssignment;
 
 async function bulkInsertNewAssignments(assignments) {
-    const assignmentsToInsert = assignments.map(function (assignment) {
-        return extractValidFields(assignment, AssignmnetSchema);
-    });
+  const assignmentsToInsert = assignments.map(function (assignment) {
+    return extractValidFields(assignment, assignmentSchema);
+  });
 
-    const db = getDbReference();
-    const collection = db.collection('Assignments');
-    const results = await collection.insertMany(assignmentsToInsert);
-    return results.insertedIds;
+  const db = getDbReference();
+  const collection = db.collection("Assignments");
+  const results = await collection.insertMany(assignmentsToInsert);
+  return results.insertedIds;
 }
 
 exports.bulkInsertNewAssignments = bulkInsertNewAssignments;
 
 async function getAssignmentsForClass(courseId) {
-    const db = getDbReference();
+  const db = getDbReference();
 
-    const assignments = await db.collection('Assignments').find({ courseId: courseId }, {projection: {courseId: 1, title: 1, points: 1, due: 1, _id: 0}}).toArray();
-    if (!assignments) {
-        console.log("NULL");
-        return null;
-    };
-    console.log("NOT NULL");
-    return assignments
+  const assignments = await db
+    .collection("Assignments")
+    .find(
+      { courseId: courseId },
+      { projection: { courseId: 1, title: 1, points: 1, due: 1, _id: 0 } }
+    )
+    .toArray();
+  if (!assignments) {
+    console.log("NULL");
+    return null;
+  }
+  console.log("NOT NULL");
+  return assignments;
 }
 
 exports.getAssignmentsForClass = getAssignmentsForClass;
 
 async function getAssignments() {
-    const db = getDbReference();
-    const collection = db.collection('Assignments');
-    return collection;
+  const db = getDbReference();
+  const collection = db.collection("Assignments");
+  return collection;
 }
 
 exports.getAssignments = getAssignments;
